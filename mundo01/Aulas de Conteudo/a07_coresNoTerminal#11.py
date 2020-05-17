405ac5dc6ccf67a333a9b0de4615c7c78ddc34ed
williancae <williancaecam@gmail.com>
2020-05-16 23:22:51 -0300

    Organizando arquivos em uma nova pasta

---
+++ mundo01/Aulas de Conteudo/a07_coresNoTerminal#11.py
@@ -0,0 +1,30 @@
+'''
+Trabalhando com Cores -- ANSI
+
+\033[style;text;background-m
+style
+0 - sem estilo
+1 - Negrito
+4 - Sublinhar
+7 - Negativo
+----------
+text
+30 - branco
+31 - vermelho
+32 - verde
+33 - amarelo
+34 - azul
+35 - roxo
+36 - azul claro
+37 - cinza
+------------
+background (fundo)
+40 - branco
+41 - vermelho
+42 - verde
+43 - amarelo
+44 - azul
+45 - roxo
+46 - azul claro
+47 - cinza
+'''
