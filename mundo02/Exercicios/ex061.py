13c76405c92d417a8d959a21b88bb9c16950937e
williancae <williancaecam@gmail.com>
2020-05-23 23:07:11 -0300

    Resolução dos exercicios 61-62

---
+++ mundo02/Exercicios/ex061.py
@@ -0,0 +1,27 @@
+# '''
+#     Crie um Programa que leia 2 valores, e faça um laço com, soma, multiplica, repetir e sair
+# '''
+# # Inicializando variaveis
+# somar = multiplicar = maior = repetir = op = 0
+# while op != 5:
+#     print('-'*5,' Operaçõs ','-'*5)
+#     op =int(input('[1] Somar'''
+#                   '[2] Multiplicar'
+#                   '[3] Maior'
+#                   '[4] Novos Valores'
+#                   '[5] Sair'))
+#     num1 = int(input('Digite o numero: '))
+#     num2 = int(input('Digite outro valor: '))
+#     if op == 1:
+#         somar = num1 + num2
+#     elif op == 2:
+#         multiplicar = num1 * num2
+#     elif op == 3:
+#         if num1 > num2:
+#             maior = num1
+#         else:
+#             maior = num2
+#     elif op == 4:
+# #         Repetir
+#     elif op == 5:
+# #         sair
