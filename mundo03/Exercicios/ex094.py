84748518752531b73380e68d4def3f62812eef28
williancae <williancaecam@gmail.com>
2020-08-09 00:41:08 -0300

    Exercico 094

---
+++ mundo03/Exercicios/ex094.py
@@ -0,0 +1,11 @@
+jogadores = {}
+golsPartidas = []
+jogadores['nome'] = str(input('Nome jogador: '))
+jogadores['partidas'] = int(input('Quantas partidas jogou: '))
+quant = jogadores['partidas']
+for i in range(0, quant):
+    golsPartidas.append(int(input(f'Gols na {i+1}° partida: ')))
+jogadores['golsPartidas'] = golsPartidas
+jogadores['totalGols'] = sum(golsPartidas)
+print(jogadores)
+
