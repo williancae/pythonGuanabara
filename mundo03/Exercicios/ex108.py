c2f6337bcd5dc1f196879272891f2f22ae226f60
williancae <williancaecam@gmail.com>
2020-08-16 20:23:23 -0300

    organizando  arquivos em pastas

--- mundo03/Exercicios/ex108.py
+++
@@ -1,6 +0,0 @@
-import ex108Moeda as a
-p = float(input('Digite um numero: '))
-print(f'O dobro de {p} é {a.dobro(p)}')
-print(f'A metade de {p} é {a.metade(p)}')
-print(f'O aumentando 10% é {a.aumenta(p, 10)}')
-print(f'O diminuido 13% é {a.diminui(p, 13)}')
