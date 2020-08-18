74ed81cb8a491fa53b5078fd1447600775bf18cd
williancae <williancaecam@gmail.com>
2020-08-17 21:49:28 -0300

    finalizado o exercicio 113

---
+++ mundo03/Aulas de Conteudo/a018_modulodePacotes#22.py
@@ -0,0 +1,7 @@
+'''
+Modularização e Epacotamento
+- Divisão de codigos em pequenos projetos
+- Separadas em pastas e com o  codigo salvo como '__init__.py'
+- Depois de ter separados funções  em  outros arquivos Py
+- São importadas no documento principla "main"
+'''
--- mundo03/Exercicios/modalarizacao/ex113/ex113.py
+++ mundo03/Exercicios/modalarizacao/ex113/ex113.py
@@ -1,3 +1,5 @@
 from utilidade import moeda as a
-p = float(input('Digite o Preço: '))
+from utilidade import dado as d
+
+p = d.leiaDinheiro('Digite o Preço: ')
 a.resumo(p, 10, 13)
